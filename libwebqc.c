b321bad9b5c4592c6908dbd637d9770f3094a236
ury <4493070+urysegal@users.noreply.github.com>
2022-07-11 20:04:22 -0700

    WIP

--- libwebqc.c
+++ libwebqc.c
@@ -16,12 +16,21 @@ webqc_return_value_t init_webqc_return_value()
     return  rv;
 }
 
+WQC *wqc_init()
+{
+    WQC *handler = malloc(sizeof (struct webqc_handler_t));
+    handler->return_value = init_webqc_return_value();
+    return handler;
+}
+
 void wqc_cleanup(WQC *handler)
 {
-    if ( handler->access_token )
-    {
-        free(handler->access_token);
-        handler->access_token = NULL;
+    if ( handler ) {
+        if (handler->access_token) {
+            free(handler->access_token);
+            handler->access_token = NULL;
+        }
+        free (handler);
     }
 }
 
@@ -30,5 +39,4 @@ void wqc_set_error(WQC *handler, error_code_t code)
     handler->return_value.error_code = code;
     const char *error_message = wqc_get_error_by_code(code);
     strncpy(handler->return_value.error_message, error_message, MAX_WEBQC_ERROR_MESSAGE_LEN);
-
 }
